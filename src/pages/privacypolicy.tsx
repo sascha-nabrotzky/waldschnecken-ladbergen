b9a7c61906ab094d2800147b435caccbe7537350
sascha-nabrotzky <sascha.nabrotzky@online.de>
2023-09-02 11:53:31 +0200

    add privacy policy

---
+++ src/pages/privacypolicy.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import type { HeadFC, PageProps } from 'gatsby';
+import MainLayout from '../components/MainLayout';
+
+const IndexPage: React.FC<PageProps> = () => {
+    return (
+        <>
+            <MainLayout>
+                <h1 className="mt-4 mb-12 font-serif text-3xl lg:text-5xl text-center">
+                    Datenschutzerklärung
+                </h1>
+                <p className="mt-12 text-xl text-center max-w-xl">
+                    Hier steht demnächst die Datenschutzerklärung
+                </p>
+            </MainLayout>
+        </>
+    );
+};
+
+export default IndexPage;
+
+// built-in Head export that allows you to add elements to the document head of your pages
+export const Head: HeadFC = () => <title>Waldschnecken | Konzept</title>;
