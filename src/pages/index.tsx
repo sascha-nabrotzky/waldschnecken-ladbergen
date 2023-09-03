82aee42daeeaa153caa1baa936de8b8f6fc8bac2
sascha-nabrotzky <sascha.nabrotzky@online.de>
2023-09-03 12:43:52 +0200

    center content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,47 +12,44 @@ const IndexPage: React.FC<PageProps> = () => {
     }
 
     return (
-        <>
-            <MainLayout>
-                <h2 className="font-serif text-2xl text-center">
-                    Hier entsteht die neue Internet-Präsenz der
-                </h2>
-                <h1 className="mt-4 mb-12 font-serif text-3xl lg:text-5xl text-center">
-                    Großtagespflege &bdquo;Waldschnecken&ldquo;
-                </h1>
-                <img
-                    src={Logo}
-                    alt="Logo der Großtagespflege Waldschnecken"
-                />
-                <p className="mt-12 text-xl text-center max-w-xl">
-                    Liebevolle und naturbezogene Betreuung Ihrer Kinder in
-                    Ladbergen durch Tatjana Nabrotzky und Oksana Welk
-                </p>
-                <address className="text-center mt-12 text-xl">
-                    Auf Stieneckers 52, <br />
-                    49549 Ladbergen <br />
-                    <a
-                        href={`${currentMail}`}
-                        target="_blank"
-                        rel="noreferrer"
-                        onClick={addMail}
-                        className="font-bold text-xl"
-                    >
-                        E-Mail schreiben
-                    </a>
-                    <br />
-                    <br />
-                    <a href={`tel: +49 170 2802494`}>
-                        Tatjana Nabrotzky: 0170 2802494
-                    </a>
-                    <br />
-                    <br />
-                    <a href={`tel: +49 162 1366853`}>
-                        Oksana Welk: 0162 1366853
-                    </a>
-                </address>
-            </MainLayout>
-        </>
+        <MainLayout>
+            <h2 className="font-serif text-2xl text-center">
+                Hier entsteht die neue Internet-Präsenz der
+            </h2>
+            <h1 className="mt-4 mb-12 font-serif text-3xl lg:text-5xl text-center">
+                Großtagespflege &bdquo;Waldschnecken&ldquo;
+            </h1>
+            <img
+                src={Logo}
+                alt="Logo der Großtagespflege Waldschnecken"
+                className="mx-auto"
+            />
+            <p className="mx-auto mt-12 text-xl text-center max-w-xl">
+                Liebevolle und naturbezogene Betreuung Ihrer Kinder in Ladbergen
+                durch Tatjana Nabrotzky und Oksana Welk
+            </p>
+            <address className="text-center mt-12 text-xl">
+                Auf Stieneckers 52, <br />
+                49549 Ladbergen <br />
+                <a
+                    href={`${currentMail}`}
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={addMail}
+                    className="font-bold text-xl"
+                >
+                    E-Mail schreiben
+                </a>
+                <br />
+                <br />
+                <a href={`tel: +49 170 2802494`}>
+                    Tatjana Nabrotzky: 0170 2802494
+                </a>
+                <br />
+                <br />
+                <a href={`tel: +49 162 1366853`}>Oksana Welk: 0162 1366853</a>
+            </address>
+        </MainLayout>
     );
 };
 
