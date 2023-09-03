aa30df85688f78322a96d55a7a71016b235e7203
sascha-nabrotzky <sascha.nabrotzky@online.de>
2023-09-03 12:43:32 +0200

    reduce font size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 const Footer: React.FC = () => {
     return (
         <footer className="relative bottom-0 right-0 left-0 p-2 bg-roman-coffee-600 text-white">
-            <nav className="flex gap-4 text-xl">
+            <nav className="flex gap-4 text-md">
                 <Link to="/imprint">Impressum</Link>
                 <Link to="/privacypolicy">Datenschutz</Link>
             </nav>
