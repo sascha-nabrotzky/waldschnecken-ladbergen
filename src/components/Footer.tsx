8ecc93e12a87bea3a40cc3172aff74b83f5aba4e
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-09-08 08:09:03 +0200

    add insta icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import { ReactComponent as WavesRight } from '../images/waves-right.svg'
+import { ReactComponent as Instagram } from '../icons/instagram.svg'
 
 const Footer: React.FC = () => {
   return (
     <footer className="relative bottom-0 right-0 left-0 ">
       <WavesRight className="text-roman-coffee-600" />
-      <div className="pr-12 pb-12 bg-roman-coffee-600 text-right text-white">
+      <div className="flex justify-between px-10 pb-12 bg-roman-coffee-600 text-right text-white">
+        <a
+          href="https://www.instagram.com/waldschnecken/"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="p-2"
+        >
+          <Instagram className="h-10" />
+        </a>
         <nav className="flex flex-col text-md">
           <Link
             to="/imprint"
@@ -26,7 +35,8 @@ const Footer: React.FC = () => {
             rel="noreferrer noopener"
             className="opacity-50"
           >
-            Webdesign & Illustration | Sascha Nabrotzky
+            <span className="whitespace-nowrap">Webdesign & Illustration</span>{' '}
+            | <span className="whitespace-nowrap">Sascha Nabrotzky</span>
           </a>
         </nav>
       </div>
