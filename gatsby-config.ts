1d80e3a443faad1b72d8d281cc76fb7c19e68073
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-09-11 16:04:32 +0200

    integrate pwa functionality

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -53,6 +53,24 @@ const config: GatsbyConfig = {
         path: './src/pages/'
       },
       __key: 'pages'
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Waldschnecken-Ladbergen',
+        short_name: 'Waldschnecken',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#bba189',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: 'use-credentials'
+      }
     }
   ]
 }
