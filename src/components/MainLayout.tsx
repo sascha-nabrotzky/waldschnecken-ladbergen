1f17e893ddae22dd327176c1f2e2125e80f364b4
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-09-14 07:49:11 +0200

    set max width to main layout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -17,7 +17,7 @@ const MainContent: React.FC<Props> = ({
   return (
     <div className="grid grid-rows-[repeat(3,_min-content)_1fr_min-content] min-h-[100dvh] text-mine-shaft-950">
       <Header />
-      <main className="p-8 w-full">
+      <main className="flex flex-col mx-auto max-w-2xl p-8 w-full">
         {pagetitle != null && (
           <h1 className="mt-4 mb-12 font-serif text-3xl lg:text-5xl text-center break-word hyphens-auto">
             {pagetitle}
